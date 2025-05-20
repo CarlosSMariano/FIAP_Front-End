d004c3be175bb032466becb33e02a657dc1c428c
CarlosSMariano <sanchesmarianocarlos@gmail.com>
2025-05-20 11:48:04 -0300

    Criando variáveis em javaScript

---
+++ Projeto5/src/JavaScript/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    // Declarando as variaveis
+    const loginForm = document.getElementById("form-login");
+    const messagem = document.getElementById('message');
+    const video = document.querySelector('.video-container > video');
+    const loginContainer = document.getElementById('login-container')
+    const buttonEntrar = document.getElementById('btnEntrar')
+    const buttonSair= document.getElementById('btnTrocar')
+
+    // Verifica se o usuario está logado no localStore
+    
+
+})
